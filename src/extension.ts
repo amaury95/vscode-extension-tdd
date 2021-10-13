3c94a731709f56eed58e4d41a1dc0ece1c45bcf1
Amaury Diaz <ad@micobo.com>
2021-10-13 11:17:37 +0200

    setup activation mechanism for the extension.

--- src/extension.ts
+++ src/extension.ts
@@ -3,13 +3,44 @@ import * as utils from "./utils";
 import testSuites from "./suites";
 
 export function activate(context: vscode.ExtensionContext) {
-  const suites = () => testSuites.filter((s) => s.isSet(context));
+  let active: boolean = false;
+
+  const suites = () =>
+    active ? testSuites.filter((s) => s.isSet(context)) : [];
 
   const testSide = () => vscode.ViewColumn.Two;
   const codeSide = () =>
     testSide() === vscode.ViewColumn.Two
       ? vscode.ViewColumn.One
       : vscode.ViewColumn.Two;
+
+  // activate button definition
+  const activateButton = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+    100
+  );
+
+  const updateActivateButton = () => {
+    // activateButton.color = active ? "#FF0000" : "#00FF00";
+    const icon = active ? "$(testing-passed-icon)" : "$(error)";
+    activateButton.text = `${icon} TDD Zen`;
+  };
+
+  // activate command
+  const activateCommandId = "tdd-zen.activate";
+  context.subscriptions.push(
+    vscode.commands.registerCommand(activateCommandId, () => {
+      active = !active;
+      updateActivateButton();
+    })
+  );
+
+  // activate functionality setup
+  activateButton.command = activateCommandId;
+  updateActivateButton();
+  activateButton.show();
+
+  context.subscriptions.push(activateButton);
 
   vscode.window.onDidChangeActiveTextEditor(async (data) => {
     if (data) {
